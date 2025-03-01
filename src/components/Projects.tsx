1e77ba9debfca53c67787b997db4a6c240e64e81
Shobhit Gupta <123019122+shobhit26-09@users.noreply.github.com>
2025-03-01 15:30:04 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,39 +4,39 @@ import { Github, ExternalLink } from "lucide-react";
 
 const projects = [
   {
-    title: "Project One",
-    description: "A full-stack web application built with React and Node.js",
+    title: "ResumeAI",
+    description: "AI powered resume builder and analyser",
     image:
-      "https://images.unsplash.com/photo-1460925895917-afdab827c52f?auto=format&fit=crop&q=80&w=800",
-    tech: ["React", "Node.js", "MongoDB"],
-    github: "#",
-    live: "#",
+      "https://i.imgur.com/Fz1QhZ9.png",
+    tech: ["TypeScript", "Node.js", "MongoDB"],
+    github: "https://github.com/shobhit26-09/ResumeAI-",
+    live: "https://resumeai26.netlify.app/",
   },
   {
-    title: "Project Two",
+    title: "Weather Website",
     description: "E-commerce platform with real-time updates",
     image:
-      "https://images.unsplash.com/photo-1555421689-491a97ff2040?auto=format&fit=crop&q=80&w=800",
+      "https://cdn.dribbble.com/userupload/10729463/file/original-b00b3b85cb3f11b53b274fbe71c85e37.png?format=webp&resize=400x300&vertical=center",
     tech: ["Next.js", "TypeScript", "Stripe"],
-    github: "#",
+    github: "https://github.com/shobhit26-09/Weather",
+    live: "shobhit26-09.github.io/Weather/",
+  },
+  {
+    title: "ML Based Home Security System",
+    description: "ML based home security system",
+    image:
+      "https://hackster.imgix.net/uploads/attachments/1608419/_IvBBo51EL6.blob?auto=compress&w=900&h=675&fit=min&fm=jpg",
+    tech: ["Python", "TensorFlow", "Pytorch"],
+    github: "https://github.com/shobhit26-09/ML-based-auth-System",
     live: "#",
   },
   {
-    title: "Project Three",
-    description: "AI-powered content management system",
+    title: "Voice Assistant",
+    description: "Voice Assistant to complete daily tasks",
     image:
-      "https://images.unsplash.com/photo-1555949963-aa79dcee981c?auto=format&fit=crop&q=80&w=800",
-    tech: ["Python", "TensorFlow", "React"],
-    github: "#",
-    live: "#",
-  },
-  {
-    title: "Project Four",
-    description: "Real-time collaboration tool for teams",
-    image:
-      "https://images.unsplash.com/photo-1551434678-e076c223a692?auto=format&fit=crop&q=80&w=800",
-    tech: ["Socket.io", "React", "Redux"],
-    github: "#",
+      "https://cdn.prod.website-files.com/5e305a6cb7083222527a89cc/66e414c27b4818a196ff0c1d_how_to_make_an_ai_voice_assistant_10_steps.webp",
+    tech: ["Python", "Pytorch", "API"],
+    github: "https://github.com/shobhit26-09/Voice-Assistant",
     live: "#",
   },
 ];
